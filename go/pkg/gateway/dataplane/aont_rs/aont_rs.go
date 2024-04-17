289ce02b93407065d50dff5961ad1211c6ea4a74
Cheng-Yen Wu <65587623+PoisWu@users.noreply.github.com>
2024-04-17 10:42:42 +0800

    Clean a bit the code, Implement RS

--- go/pkg/gateway/dataplane/aont_rs/aont_rs.go
+++ go/pkg/gateway/dataplane/aont_rs/aont_rs.go
@@ -9,9 +9,14 @@ import (
 	"encoding/binary"
 	"github.com/andreburgaud/crypt2go/padding"
 	"crypto/sha256"
+	"github.com/klauspost/reedsolomon"
+	"reflect"
+
 )
 
-
+var padder = padding.NewPkcs7Padding(16)
+var canary_block []byte = []byte{0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
+var rs_enc, _ =  reedsolomon.New(4,2)
 
 func AONT_Encode(message []byte) []byte{
 	// Use the notation of the paper AONT-RS
@@ -30,7 +35,6 @@ func AONT_Encode(message []byte) []byte{
 
 
 	// Padding message into multiple of 16 Using PKC#7
-	padder := padding.NewPkcs7Padding(16)
 	p_message, err := padder.Pad(message)
 	if err != nil{
 		panic(err)
@@ -42,6 +46,7 @@ func AONT_Encode(message []byte) []byte{
 	// d0, d1,...ds-1, ds(canary), ds+1, ds+2
 	encoded_text := make([]byte, len(p_message) + 3*aes.BlockSize)
 	copy(encoded_text, p_message)
+	copy(encoded_text[len(p_message):], canary_block)
 
 
 	// encoding
@@ -58,22 +63,30 @@ func AONT_Encode(message []byte) []byte{
 		}
 	}
 	// compute c_{s+1}
-	h := sha256.New()
-	h.Write(encoded_text[:(s+1)*aes.BlockSize])
-	copy(encoded_text[(s+1)*aes.BlockSize:], h.Sum(nil))
+	var hash_value = sha256.Sum256(encoded_text[:(s+1)*aes.BlockSize])
+	copy(encoded_text[(s+1)*aes.BlockSize:], hash_value[:])
 
 	for j:=0; j < 2*aes.BlockSize; j++ {
 		encoded_text[(s+1)*aes.BlockSize+j] ^= key[j]
 	}
 	return encoded_text
-
 }
+
+func RS_Encode(encoded_text []byte) [][]byte{
+	shards, _ := rs_enc.Split(encoded_text)
+	_ = rs_enc.Encode(shards)
+	ok, _ := rs_enc.Verify(shards)
+	if ok {
+		fmt.Println("ok")
+	}
+	return shards
+}
+
+
 
 func AONT_Decode(encoded_text []byte) []byte {
 	s := len(encoded_text) / aes.BlockSize - 3
-	h := sha256.New()
-	h.Write(encoded_text[:(s+1)*aes.BlockSize])
-	hash_document := h.Sum(nil)
+	hash_document := sha256.Sum256(encoded_text[:(s+1)*aes.BlockSize])
 	for j:=0; j < 2*aes.BlockSize; j++ {
 		encoded_text[(s+1)*aes.BlockSize+j] ^= hash_document[j]
 	}
@@ -83,7 +96,7 @@ func AONT_Decode(encoded_text []byte) []byte {
 	}
 
 
-	// encoding
+	// decoding
 	for i:=0; i<=s; i++ {
 		mask := make([]byte, aes.BlockSize)
 		binary.BigEndian.PutUint64(mask, uint64(i+1))
@@ -97,7 +110,12 @@ func AONT_Decode(encoded_text []byte) []byte {
 		}
 	}
 
-	padder := padding.NewPkcs7Padding(16)
+	// veryfy canary
+	if !reflect.DeepEqual(encoded_text[s*aes.BlockSize:(s+1)*aes.BlockSize],
+		canary_block){
+		fmt.Println("Canary integrety not good")
+	}
+
 	message, err := padder.Unpad(encoded_text[:s*aes.BlockSize])
 	if err != nil{
 		panic(err)
@@ -107,76 +125,15 @@ func AONT_Decode(encoded_text []byte) []byte {
 
 }
 
+// func RS_Decode(encoded_shard [][]byte) []byte{
+// 	return "
+// }
+
 func main(){
 	plaintext := []byte("some plaintext")
 	encoded := AONT_Encode(plaintext)
+	fmt.Println(RS_Encode(encoded))
 	fmt.Println(encoded)
 	fmt.Println(plaintext)
 	fmt.Println(AONT_Decode(encoded))
 }
-
-// func AONT_RSDecode(bytes []byte) []byte{
-// }
-
-// func AONTEncode(bytes []byte) []byte {
-// 	n := len(bytes)
-// 	if n <= 1 {
-// 		// AONT is not applied
-// 		return bytes
-// 	}
-//
-// 	// Default: GF(256)
-// 	// a in GF(256) where a^2 = a + 1
-// 	// AONT encoder is actually a matrix muliplication:
-// 	// | y1 |   | 1 0 ... 0 1 | | x1 |
-// 	// | y2 |   | 0 1 ... 0 1 | | x2 |
-// 	// | y3 | = | ... ... ... | | x3 |
-// 	// | .. |   | 0 0 ... 1 1 | | .. |
-// 	// | yn |   | 1 1 ... 1 a | | xn |
-// 	cum := GF.Mul(GF_a, bytes[n-1])
-// 	for i := 0; i < n-1; i++ {
-// 		cum = GF.Add(cum, bytes[i])
-// 		bytes[i] = GF.Add(bytes[i], bytes[n-1])
-// 	}
-// 	bytes[n-1] = cum
-// 	return bytes
-// }
-//
-// func AONTDecode(bytes []byte) []byte {
-// 	n := len(bytes)
-// 	if n <= 1 {
-// 		// AONT is not applied
-// 		return bytes
-// 	}
-//
-// 	// Default: GF(256)
-// 	// a, b in GF(256) where b = a^2 = a + 1
-// 	// Similarly, we also have a = b^2 = b + 1
-// 	// AONT decoder is actually a matrix muliplication:
-// 	// Case 1: n is even
-// 	// | x1 |   | b a ... a a | | y1 |   | a a ... a a | | y1 |   | y1 |
-// 	// | x2 |   | a b ... a a | | y2 |   | a a ... a a | | y2 |   | y2 |
-// 	// | x3 | = | ... ... ... | | y3 | = | ... ... ... | | y3 | + | y3 |
-// 	// | .. |   | a a ... b a | | .. |   | a a ... a a | | .. |   | .. |
-// 	// | xn |   | a a ... a a | | yn |   | a a ... a a | | yn |   | 0  |
-// 	// Case 2: n is odd
-// 	// | x1 |   | a b ... b b | | y1 |   | b b ... b b | | y1 |   | y1 |
-// 	// | x2 |   | b a ... b b | | y2 |   | b b ... b b | | y2 |   | y2 |
-// 	// | x3 | = | ... ... ... | | y3 | = | ... ... ... | | y3 | + | y3 |
-// 	// | .. |   | b b ... a b | | .. |   | b b ... b b | | .. |   | .. |
-// 	// | xn |   | b b ... b b | | yn |   | b b ... b b | | yn |   | 0  |
-// 	cum := byte(0)
-// 	for i := 0; i < n; i++ {
-// 		cum = GF.Add(cum, bytes[i])
-// 	}
-// 	if n%2 == 0 {
-// 		cum = GF.Mul(cum, GF_a)
-// 	} else {
-// 		cum = GF.Mul(cum, GF_b)
-// 	}
-// 	for i := 0; i < n-1; i++ {
-// 		bytes[i] = GF.Add(cum, bytes[i])
-// 	}
-// 	bytes[n-1] = cum
-// 	return bytes
-// }
--- go/pkg/gateway/dataplane/aont_rs/go.mod
+++ go/pkg/gateway/dataplane/aont_rs/go.mod
@@ -3,3 +3,9 @@ module example/aont-rs
 go 1.22.2
 
 require github.com/andreburgaud/crypt2go v1.5.0
+
+require (
+	github.com/klauspost/cpuid/v2 v2.2.7 // indirect
+	github.com/klauspost/reedsolomon v1.12.1 // indirect
+	golang.org/x/sys v0.19.0 // indirect
+)
